4b23131438bb0757482415e124aaf52848d32c30
gustav474 <gustav474@gmail.com>
2018-09-17 19:30:33 +0400

    Commit

--- testapp/views.py
+++ testapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 import requests
 
-client_id = 6689761
-redirect_uri = 'http://gusstav474.pythonanywhere.com/redirect'
-display = 'page'
-scope = 'friends'
-response_type = 'token'
+# client_id = 6689761
+# redirect_uri = 'http://gusstav474.pythonanywhere.com/redirect'
+# display = 'page'
+# scope = 'friends'
+# response_type = 'token'
 v = 5.84
 
 def index(request):
@@ -15,6 +15,6 @@ def index(request):
     payloads = {'user_id': user_id, 'access_token': access_token, 'order': 'random', 'fields': 'nickname',
                     'count': 5, 'v': v}
     response = requests.get('https://api.vk.com/method/friends.get', params=payloads)
-    friends = response.json()['response']['items']
+    friends = response.json().get('response')['items']
 
     return render(request, 'test/index.html', {'friends': friends})
